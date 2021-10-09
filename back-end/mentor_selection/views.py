2fbee5be366be7f67861b8e90570a2fcc1385c3a
Dmitrii Chebotarev <skrubberqumo@gmail.com>
2021-10-09 15:22:11 +0300

    feat(Adds-docker-compose-usage-with-gunicorn-WSGI,-pre-commit-hook,-git-workflow,-poetry-files): fixes files to run within docker container and according to black and isort checks, adds static/admin for gunicorn, poetry files and .ignore files

---
+++ back-end/mentor_selection/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Страница приложения mentor_selection")
+
+
+# Create your views here.
--- mentors_site/mentor_selection/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MentorSelectionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mentor_selection'
