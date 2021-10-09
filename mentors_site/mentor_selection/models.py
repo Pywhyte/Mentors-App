9cc5ac52b66feb98b0e24ebce4637c4b6e5682d1
Mazhorova <mazhorova.inna@yandex.ru>
2021-10-09 09:34:07 +0300

    classes and initial_migration_for_table_creation_mazhorova

--- mentors_site/mentor_selection/models.py
+++ mentors_site/mentor_selection/models.py
@@ -1,6 +1,5 @@
 import datetime
 from datetime import timedelta
-from typing import Union
 
 from django.db import models
 from django.utils import timezone
@@ -10,10 +9,75 @@ class DeadlineError(Exception):
     pass
 
 
-class Users(models.Model):
+class Organization(models.Model):
+    org_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=50)
+    url = models.CharField(max_length=50)
+    contact_number = models.CharField(max_length=50)
+
+
+class User(models.Model):
+    us_id = models.AutoField(primary_key=True)
     last_name = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
+    organization_id = models.ForeignKey(
+        Organization, on_delete=models.SET_NULL, null=True
+    )
+    e_mail = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return f"{self.last_name} {self.first_name}"
 
+    def become_mentor(self, main_skill, skill_level):
+        return Mentor(user_id=self, main_skill=main_skill, skill_level=skill_level)
+
+
+class Student(User):
+    stud_id = models.AutoField(primary_key=True)
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+SKILLS_CHOICES = (
+    ("JAVA_SCRIPT", "JAVA"),
+    ("DATA_SCIENCE", "data_science"),
+    ("PYTHON", "python"),
+    ("AUTOMATIC_TESTING", "automatic_testing"),
+)
+
+
+SKILL_LEVEL = (
+    ("LOW", "low"),
+    ("MIDDLE", "middle"),
+    ("HIGH", "high"),
+)
+
+
+class Mentor(User):
+    ment_id = models.AutoField(primary_key=True)
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    main_skill = models.CharField(
+        max_length=50, choices=SKILLS_CHOICES, default="PYTHON"
+    )
+
+    skill_level = models.CharField(max_length=50, choices=SKILL_LEVEL, default="PYTHON")
+
+
+class Request(User):
+    request_id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    mentor_id = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField()
+    period_days = models.DurationField()
+
+    desired_skill = models.CharField(
+        max_length=50, choices=SKILLS_CHOICES, default="PYTHON"
+    )
+
+    desired_skill_level = models.CharField(
+        max_length=50, choices=SKILL_LEVEL, default="PYTHON"
+    )
+
+    accepted = models.BooleanField()
