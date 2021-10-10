dc222a046d472513bd9cb64385897866a2a539cd
wellsi <wellsi@bk.ru>
2021-10-11 02:23:29 +0300

    front

--- src/components/userItem.jsx
+++ src/components/userItem.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useHistory } from 'react-router';
 import MyButton from './button/MyButton';
 import "./Styles.css"
+import ava from "../Images/user.png"
 
 const UserItem = (props) => {
-
 
 
     const router = useHistory()
@@ -12,15 +12,15 @@ const UserItem = (props) => {
     return (
         <div className="user">
             <div  className="user__navbar">
-            <img className="user__avatar" src={props.post.avatar_url} alt="logo" width="90" height="90"/>
+            <img className="user__avatar" src="https://pbs.twimg.com/media/DYfwnKOXUAEgTc2.jpg" alt="logo" width="90" height="90"/>
             </div>
             <div className="user__content">
                 <strong className="item__1"> {props.post.name}</strong>
                 <div  className="item__1">
-                    {props.post.company.name}
+                    {props.post.email}
                 </div>
-                <div  className="item__1">{props.post.post.skill_level}</div>
-                <div  className="item__1">{props.post.organization_id.company_name}</div>
+                <div  className="item__1">{props.post.company.name}</div>
+                <div  className="item__1">{props.post.company.catchPhrase}</div>
             </div>
             <div className="user__btns">
                 <MyButton
